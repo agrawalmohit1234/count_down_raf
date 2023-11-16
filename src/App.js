ac848567d45d69c1d7fd497c1a4cdc74f0cc419c
unknown <rajagrawalmohit2@gmail.com>
2023-11-16 20:44:20 +0530

    Count Down using requestAnimationFrame

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [minute, setMinute] = useState(null);
+  const [second, setSecond] = useState(null);
+  const [updateTime, setUpdateTime] = useState(time);
+
+  let startTime;
+  function startAnimation(timestamp) {
+    if (!startTime) {
+      startTime = timestamp;
+    }
+    const elapsedTime = timestamp - startTime;
+    const remainingTime = Math.max(0, updateTime * 1000 - elapsedTime);
+    const seconds = Math.ceil(remainingTime / 1000);
+    let [m, s] = convertToMinuteSec(seconds);
+    setMinute(m);
+    setSecond(s);
+    setUpdateTime((t) => seconds);
+    if (remainingTime > 0) {
+      requestAnimationFrame(startAnimation);
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      requestAnimationFrame(startAnimation);
+    }, 0);
+  }, [startAnimation]);
+
+  const handleChange = (e) => {
+    setTime(e.target.value);
+  };
+
+  const handleCountDown = () => {
+    setUpdateTime(time);
+  };
+
+  const convertToMinuteSec = (t) => {
+    return [Math.floor(t / 60), t % 60];
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="inputData">
+        <input
+          type="number"
+          value={time}
+          onChange={handleChange}
+          placeholder="Seconds...."
+        />
+        <button onClick={handleCountDown}>Start Count Down</button>
+        {(second > 0 || minute > 0) && (
+          <div className="countdown">
+            <div>
+              {`${String(minute).padStart(2, "0")} : ${String(second).padStart(
+                2,
+                "0"
+              )}`}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
